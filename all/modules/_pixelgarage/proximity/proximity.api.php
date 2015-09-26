66dda06086a3e14e633013f8175a43b86690767f
Ralph Moser <ralph@ramosoft.ch>
2015-09-26 22:50:47 +0200

    - first version Core->7.39

---
+++ all/modules/_pixelgarage/proximity/proximity.api.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Proximity module hooks.
+ *
+ * User: ralph
+ * Date: 22.04.15
+ */
+
+/**
+ * Alters the proximity item load parameter array.
+ *
+ * The load parameter array defines a specific url parameter for each proximity item.
+ * This parameter is added at the end of the ajax url and must be unique and URL conform.
+ * The unique parameter defines, which item content has to be loaded from
+ * the server (see next api function).
+ *
+ * @param $container_index      int     Index of proximity container (if more than one container exists in one page).
+ * @param $view_result          array   The result array of the view.
+ * @param $ajax_load_params     array   Array of ajax load parameters to be altered, one for each proximity item (row)
+ *                                      retrieved by the view. Default is the views row index.
+ */
+function hook_proximity_ajax_load_params_alter($container_index, $view_result, &$ajax_load_params) {
+  // Example: the view retrieves nodes as proximity items.
+  // Return the node id as the load parameter for each item.
+  foreach ($view_result as $id => $item) {
+    $ajax_load_params[$id] = $item->nid;
+  }
+}
+
+/**
+ * Returns the item specific content as render array or html string.
+ *
+ * The parameter $param contains the unique load parameter of the requested item.
+ *
+ * @param $container_index      int     Index of proximity container (if more than one container exists in one page).
+ * @param $param                string  The item specific load parameter (see also hook_proximity_ajax_load_params_alter).
+ * @param $render_item          mixed   The rendered content to be returned to the client. The $render_item should be
+ *                                      replaced either by a string (rendered html content), a render array or an integer (error code).
+ */
+function hook_proximity_ajax_render_item_alter($container_index, $param, &$render_item) {
+  // Example: the $param variable contains a node id (see hook_proximity_ajax_load_params_alter),
+  // return the render array for the specific node
+  if ($node= node_load($param)) {
+    $render_item = node_view($node);
+  }
+}
---
+++ all/modules/_pixelgarage/proximity/views-view-proximity.tpl.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * @file
+ * View theme template to layout the proximity items and add the bootstrap modal dialog functionality to each item.
+ */
+?>
+
+<div id="<?php print $container_id; ?>" class="pe-container">
+  <div class="pe-background-image"></div>
+
+  <?php foreach ($rows as $id => $row): ?>
+
+    <div class="pe-item pe-item-ajax <?php print 'pe-item-' . $ajax_load_params[$id]; ?> <?php if ($classes_array[$id]) print $classes_array[$id]; ?>" style="margin-left: -4px; width: <?php print $percentage_width; ?>%">
+      <div class="pe-item-inner">
+        <!-- modal trigger -->
+        <a class="btn" role="button" href="<?php print $item_base_url . $ajax_load_params[$id]; ?>" data-ajax-load-param="<?php print $ajax_load_params[$id]; ?>" data-toggle="modal" data-target="#pe-modal-dialog-<?php print $container_index; ?>" >
+          <?php print $row; ?>
+        </a>
+      </div>
+    </div>
+
+  <?php endforeach; ?>
+
+  <!--
+  Modal dialog displaying the item content
+  The item content is retrieved via AJAX or added directly on full page loads
+  -->
+  <div id="pe-modal-dialog-<?php print $container_index; ?>" class="modal" tabindex="-1" role="dialog" aria-labelledby="peModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <!-- Header -->
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <?php if ($title): ?>
+            <h2 class="modal-title" id="peModalLabel"><?php print $title; ?></h2>
+          <?php endif; ?>
+        </div>
+        <!-- Body -->
+        <div class="modal-body">
+          <?php if ($rendered_item) print $rendered_item ; ?>
+        </div>
+        <!-- Footer -->
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal"><?php print $close_text; ?></button>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+  <!--
+  Content container (used for page transitions)
+  The content for the body is retrieved via AJAX
+  <div id="pe-content-container-<?php print $container_index; ?>" class="pe-content-container" role="page">
+    <div class="content"></div>
+  </div>
+  -->
+
+</div>
