b2523ec9fa79c251864c3fb1125e87aa25e88140
Ralph Moser <ralph@ramosoft.ch>
2015-09-27 23:50:02 +0200

    - style implemented - db updated

--- all/themes/pixelgarage/templates/page.tpl.php
+++ all/themes/pixelgarage/templates/page.tpl.php
@@ -118,60 +118,60 @@
   </div>
 </header>
 
-<div class="main-container container">
+<div class="main-container">
+  <div class="container">
+    <header role="banner" id="page-header">
+      <?php print render($page['header']); ?>
+    </header> <!-- /#page-header -->
 
-  <header role="banner" id="page-header">
-    <?php print render($page['header']); ?>
-  </header> <!-- /#page-header -->
+    <div class="row">
 
-  <div class="row">
-
-    <?php if (!empty($page['sidebar_first'])): ?>
-      <aside class="col-sm-3" role="complementary">
-        <?php print render($page['sidebar_first']); ?>
-      </aside>  <!-- /#sidebar-first -->
-    <?php endif; ?>
-
-    <section<?php print $content_column_class; ?>>
-      <?php if (!empty($page['highlighted'])): ?>
-        <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
+      <?php if (!empty($page['sidebar_first'])): ?>
+        <aside class="col-sm-3" role="complementary">
+          <?php print render($page['sidebar_first']); ?>
+        </aside>  <!-- /#sidebar-first -->
       <?php endif; ?>
-      <!-- Breadcrumb -->
-      <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
-      <!-- /#skip-link destination -->
-      <a id="main-content"></a>
 
-      <!-- Main title -->
-      <?php print render($title_prefix); ?>
-      <?php if (!empty($title)): ?>
-        <h1 class="page-header"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <!-- Messages -->
-      <?php print $messages; ?>
-      <!-- Admin Tabs -->
-      <?php if (!empty($tabs)): ?>
-        <?php print render($tabs); ?>
-      <?php endif; ?>
-      <!-- Help region -->
-      <?php if (!empty($page['help'])): ?>
-        <?php print render($page['help']); ?>
-      <?php endif; ?>
-      <!-- Action links -->
-      <?php if (!empty($action_links)): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <!-- Main content -->
-      <?php print render($page['content']); ?>
-    </section>
+      <section<?php print $content_column_class; ?>>
+        <?php if (!empty($page['highlighted'])): ?>
+          <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
+        <?php endif; ?>
+        <!-- Breadcrumb -->
+        <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
+        <!-- /#skip-link destination -->
+        <a id="main-content"></a>
 
-    <?php if (!empty($page['sidebar_second'])): ?>
-      <aside class="col-sm-3" role="complementary">
-        <?php print render($page['sidebar_second']); ?>
-      </aside>  <!-- /#sidebar-second -->
-    <?php endif; ?>
+        <!-- Main title -->
+        <?php print render($title_prefix); ?>
+        <?php if (!empty($title)): ?>
+          <h1 class="page-header"><?php print $title; ?></h1>
+        <?php endif; ?>
+        <?php print render($title_suffix); ?>
+        <!-- Messages -->
+        <?php print $messages; ?>
+        <!-- Admin Tabs -->
+        <?php if (!empty($tabs)): ?>
+          <?php print render($tabs); ?>
+        <?php endif; ?>
+        <!-- Help region -->
+        <?php if (!empty($page['help'])): ?>
+          <?php print render($page['help']); ?>
+        <?php endif; ?>
+        <!-- Action links -->
+        <?php if (!empty($action_links)): ?>
+          <ul class="action-links"><?php print render($action_links); ?></ul>
+        <?php endif; ?>
+        <!-- Main content -->
+        <?php print render($page['content']); ?>
+      </section>
 
-  </div>
+      <?php if (!empty($page['sidebar_second'])): ?>
+        <aside class="col-sm-3" role="complementary">
+          <?php print render($page['sidebar_second']); ?>
+        </aside>  <!-- /#sidebar-second -->
+      <?php endif; ?>
+
+    </div>  </div>
 </div>
 <footer class="footer container">
   <?php print render($page['footer']); ?>
