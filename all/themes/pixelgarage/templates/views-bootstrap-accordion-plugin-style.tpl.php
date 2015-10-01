0bb762e9d0a07a7744974a721a57871ac1170577
Ralph Moser <ralph@ramosoft.ch>
2015-10-01 19:30:27 +0200

    - input André second part implemented - content filled - db updated

--- all/themes/pixelgarage/templates/views-bootstrap-accordion-plugin-style.tpl.php
+++ all/themes/pixelgarage/templates/views-bootstrap-accordion-plugin-style.tpl.php
@@ -7,11 +7,13 @@
             <?php if ($toggling[$key]): ?>data-toggle="collapse"<?php endif; ?>>
             <?php if (!empty($time)): ?>
               <span class="tagungs-thema-time"><?php print $time[$key] ?></span>
+            <?php elseif (!empty($first_column)): ?>
+              <span class="tagungs-thema-first <?php if ($toggling[$key]): ?>expandable<?php endif; ?>"><?php print $first_column[$key] ?></span>
             <?php endif; ?>
             <span class="tagungs-thema-title <?php if ($toggling[$key]): ?>expandable<?php endif; ?>">
               <?php print $titles[$key] ?>
-              <?php if ($subtitle[$key]): ?><div class="subtitle"><?php print $subtitle[$key] ?></div><?php endif; ?>
-              <?php if ($referent[$key]): ?><div class="referent"><?php print $referent[$key] ?></div><?php endif; ?>
+              <?php if (!empty($subtitle) && !empty($subtitle[$key])): ?><div class="subtitle"><?php print $subtitle[$key] ?></div><?php endif; ?>
+              <?php if (!empty($referent) && !empty($referent[$key])): ?><div class="referent"><?php print $referent[$key] ?></div><?php endif; ?>
             </span>
           </a>
         </h4>
